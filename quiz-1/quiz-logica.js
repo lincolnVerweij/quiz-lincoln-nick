072fe6e7932e90a362cdde64d085d9656a5953c9
lincolnVerweij <weijver.tv@gmail.com>
2026-09-11 12:47:04 +0200

    Quiz1-logica gefixt

--- quiz-1/quiz-logica.js
+++ quiz-1/quiz-logica.js
@@ -1,2 +1,11 @@
 console.log("JS successvol gekoppeld");
 
+let score = 0;
+
+function vraagCorrect() {
+    score = score + 1; 
+}
+
+function result() {
+    console.log("Correcte vragen: " + score);
+}
