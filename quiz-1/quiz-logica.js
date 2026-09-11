78f7cff1d9b4c28b7dc410b7640a775936c851e3
NickUbert-school <302258450@student.rocmondriaan.nl>
2026-09-11 12:58:54 +0200

    didi

--- quiz-1/quiz-logica.js
+++ quiz-1/quiz-logica.js
@@ -1,1 +1,11 @@
-console.log("hallo");
+console.log("JS successvol gekoppeld");
+
+let score = 0;
+
+function vraagCorrect() {
+    score = score + 1; 
+}
+
+function result() {
+    console.log("Correcte vragen: " + score);
+}
